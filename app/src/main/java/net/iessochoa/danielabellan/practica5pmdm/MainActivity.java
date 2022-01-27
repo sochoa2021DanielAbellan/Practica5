43fb70c03539ee6175e901c8d6b88ff3645b4e61
DanielAZ <danielabezarate@gmail.com>
2022-01-27 16:26:45 +0100

    Inicialización del proyecto

---
+++ app/src/main/java/net/iessochoa/danielabellan/practica5pmdm/MainActivity.java
@@ -0,0 +1,14 @@
+package net.iessochoa.danielabellan.practica5pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
