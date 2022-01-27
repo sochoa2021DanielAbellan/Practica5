f7c936e552b797b6f2547e0a6fb32894fbdb2194
DanielAZ <danielabezarate@gmail.com>
2022-01-27 19:00:03 +0100

    Hecho hasta el punto 17 de la Parte 1

--- app/src/main/java/net/iessochoa/danielabellan/practica5pmdm/MainActivity.java
+++ app/src/main/java/net/iessochoa/danielabellan/practica5pmdm/MainActivity.java
@@ -1,8 +1,10 @@
 package net.iessochoa.danielabellan.practica5pmdm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creamos y asignamos la Toolbar que hemos creado en la Actividad.
+        Toolbar miToolbar = findViewById(R.id.miToolbar);
+        setSupportActionBar(miToolbar);
     }
+
+    /*
+    *Este código es necesario para que se muestre nuestro Menú previamente creado en la parte superior de la pantalla
+    */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+
+
 }
