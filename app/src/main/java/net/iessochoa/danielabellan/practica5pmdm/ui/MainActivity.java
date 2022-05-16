c622fd46b5ec66b49dd1e3cf80cf98055d1b10e9
DanielAZ <danielabezarate@gmail.com>
2022-05-16 20:46:48 +0200

    Realizado hasta el punto 14 de la Parte 2

--- app/src/main/java/net/iessochoa/danielabellan/practica5pmdm/ui/MainActivity.java
+++ app/src/main/java/net/iessochoa/danielabellan/practica5pmdm/ui/MainActivity.java
@@ -3,10 +3,13 @@ package net.iessochoa.danielabellan.practica5pmdm.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -16,8 +19,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import net.iessochoa.danielabellan.practica5pmdm.R;
 import net.iessochoa.danielabellan.practica5pmdm.model.DiaDiario;
+import net.iessochoa.danielabellan.practica5pmdm.viewmodels.DiarioViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DiarioViewModel diarioViewModel;
 
     private AppBarLayout appBarLayout;
     private Toolbar miToolbar;
@@ -30,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        diarioViewModel= new ViewModelProvider(this).get(DiarioViewModel.class);
+        diarioViewModel.getAllDias().observe(this, new
+                Observer<List<DiaDiario>>() {
+                    @Override
+                    public void onChanged(List<DiaDiario> diario) {
+                        Log.d("P5","tamaño: "+diario.size());
+                    }
+        });
+
         setContentView(R.layout.activity_main);
 
         appBarLayout = findViewById(R.id.appBarLayout);
